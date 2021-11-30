f39b2f0584f84e33d9967e7c08958f5956a20592
fareed <fqararyah@gmail.com>
2021-11-30 21:11:13 +0100

    a

--- code_gen_CAM_basic_v2.py
+++ code_gen_CAM_basic_v2.py
@@ -11,7 +11,7 @@ uniques = []
 unique_counts = [0] * 364
 for i in range(364):
     uniques.append({})
-with open('pattern_match_snort3_content.txt', 'r') as f:
+with open('pattern_match_snort3_content_lt4.txt', 'r') as f:
     for line in f:
         line = line.replace('\n', '')
         pattern_list.append(line)
@@ -58,7 +58,8 @@ with open('./patterns_matcher.cpp', 'w') as f:
         for character, bool_variable_name in unique_map.items():
             if character in specials.keys():
                 character = specials[character]
-            f.write('boolean ' + bool_variable_name + ' =(' 'buffer[' + str(i) + "] >> 1);\n")
+            f.write('bool tmp_' + bool_variable_name + ' =(' 'buffer[' + str(i) + "] == '" + character + "');\n")
+            f.write('boolean ' + bool_variable_name + ' = tmp_' + bool_variable_name + '>>1;\n')
         
     for i in range(num_of_patterns):
         f.write('if(')
